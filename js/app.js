1ca8151c137b0f3684db95a283461b3bc78c5edd
Max Turpeinen <maxturp@hotmail.com>
2016-02-17 15:54:36 +0100

    added some views

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
 $(function() {
 	//We instantiate our model
-	var model = new DinnerModel();
+	//var model = new DinnerModel();
 	
 	//And create the needed controllers and views
-	var exampleView = new ExampleView($("#root"));
-	var controller = new Controller($("#root"));
+	//var exampleView = new ExampleView($("#root"));
+	//var controller = new Controller($("#root"));
 	
 	//once index has finished loadings
 	//controller.loadlist();
 	
-	//$("#root").load("view/home_view.html");
+	$("#root").load("view/overview_view.html");
 });
